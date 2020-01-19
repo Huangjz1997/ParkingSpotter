8d3ffcb5a41ed00b65875330544749f497b592af
Zishan <liuz6@rose-hulman.edu>
2020-01-19 18:08:41 -0500

    add register

--- app/src/main/java/edu/rosehulman/parkingspotter/LoginPage.kt
+++ app/src/main/java/edu/rosehulman/parkingspotter/LoginPage.kt
@@ -1,12 +1,20 @@
 package edu.rosehulman.parkingspotter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        var button: Button =  findViewById(R.id.register)
+        button.setOnClickListener(){
+            val myIntent = Intent(this, RegisterPage::class.java)
+            startActivity(myIntent)
+        }
     }
 }
---
+++ app/src/main/java/edu/rosehulman/parkingspotter/RegisterPage.kt
@@ -0,0 +1,12 @@
+package edu.rosehulman.parkingspotter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+    }
+}
