1ce80880bd7e2153fbdeafe51fed1139afac17e6
jz <huangjz97@outlook.com>
2020-01-19 17:49:26 -0500

    login activity

--- app/src/main/java/edu/rosehulman/parkingspotter/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/parkingspotter/MainActivity.kt
@@ -1,12 +1,21 @@
 package edu.rosehulman.parkingspotter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.welcome_page)
+
+        var button: Button =  findViewById(R.id.start)
+        button.setOnClickListener(){
+            val myIntent = Intent(this, LoginPage::class.java)
+            startActivity(myIntent)
+        }
     }
 }
