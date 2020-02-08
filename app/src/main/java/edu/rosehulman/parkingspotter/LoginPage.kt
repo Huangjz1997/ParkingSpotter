6a5f18794d435daaa769ee5fea2c5b1d255292db
Zishan <liuz6@rose-hulman.edu>
2020-02-08 16:17:53 -0500

    add login

--- app/src/main/java/edu/rosehulman/parkingspotter/LoginPage.kt
+++ app/src/main/java/edu/rosehulman/parkingspotter/LoginPage.kt
@@ -1,15 +1,35 @@
 package edu.rosehulman.parkingspotter
-
+import android.app.PendingIntent.getActivity
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login_page.*
 
 class LoginPage : AppCompatActivity() {
+
+    lateinit var authListener: FirebaseAuth.AuthStateListener
+    val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        lateinit var uid : String
+        authListener = FirebaseAuth.AuthStateListener { auth: FirebaseAuth ->
+            val user = auth.currentUser
+            if (user == null) {
+//                val myIntent = Intent(this, LoginPage::class.java)
+//                startActivity(myIntent)
+            }
+            else {
+                uid = user.uid
+                val myIntent = Intent(this, ContentHolder::class.java)
+                startActivity(myIntent)
+            }
+        }
 
         var button: Button =  findViewById(R.id.register)
         button.setOnClickListener(){
@@ -18,10 +38,33 @@ class LoginPage : AppCompatActivity() {
         }
 
         var button1: Button =  findViewById(R.id.login)
-        button1.setOnClickListener(){
 
+        button1.setOnClickListener(){
+            val username = username.text;
+            val password = password.text;
+
+            login(username.toString(),password.toString())
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener(authListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener(authListener)
+
+    }
+
+    fun login(username:String, password: String){
+        auth.signInWithEmailAndPassword(username, password).addOnSuccessListener {
             val myIntent = Intent(this, ContentHolder::class.java)
+//            myIntent.putExtra("auth", auth);
             startActivity(myIntent)
+        }.addOnFailureListener{
+            Toast.makeText(this,"Wrong email/password combination!", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/edu/rosehulman/parkingspotter/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/parkingspotter/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.rosehulman.parkingspotter
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,4 +22,24 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+
+            R.id.log_out -> {
+//                auth.signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
 }
