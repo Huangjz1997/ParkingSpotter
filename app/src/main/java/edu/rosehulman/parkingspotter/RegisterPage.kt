9114faed0c0d6e41f22d12356bb7c50d6fcfe48f
zisaan <zisaanlau@gmail.com>
2020-02-17 14:41:06 -0500

    small fix

--- app/src/main/java/edu/rosehulman/parkingspotter/RegisterPage.kt
+++ app/src/main/java/edu/rosehulman/parkingspotter/RegisterPage.kt
@@ -6,16 +6,39 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.activity_login_page.*
 import kotlinx.android.synthetic.main.activity_register_page.*
 
 class RegisterPage : AppCompatActivity() {
     lateinit var authListener: FirebaseAuth.AuthStateListener
     val auth = FirebaseAuth.getInstance()
+    private var userRef = FirebaseFirestore.getInstance().collection("Users")
+    private var userList = ArrayList<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
         supportActionBar!!.hide()
+
+        userRef
+            .addSnapshotListener { snapshot: QuerySnapshot?, exception: FirebaseFirestoreException? ->
+                if (exception != null) {
+                }
+                for (docChange in snapshot!!.documentChanges) {
+                    val user = User.fromSnapshot(docChange.document)
+                    when (docChange.type) {
+                        DocumentChange.Type.ADDED -> {
+                            userList.add(0, user)
+//                        notifyItemInserted(0)
+                        }
+
+                    }
+                }
+            }
 
         var button: Button = findViewById(R.id.registeruser)
         button.setOnClickListener() {
@@ -42,11 +65,15 @@ class RegisterPage : AppCompatActivity() {
     }
 
     fun register(username: String, password: String) {
-        auth.createUserWithEmailAndPassword(username, password).addOnSuccessListener {
-            val myIntent = Intent(this, ContentHolder::class.java)
-            myIntent.putExtra("uid", it.user!!.uid);
-            myIntent.putExtra("email", it.user!!.email);
-            startActivity(myIntent)
+        auth.createUserWithEmailAndPassword(username, password).addOnSuccessListener { authResult ->
+            userRef.add(User(authResult.user!!.uid, authResult.user!!.email!!))
+                .addOnCompleteListener {
+                    val myIntent = Intent(this, ContentHolder::class.java)
+                    myIntent.putExtra("uid", authResult.user!!.uid);
+                    myIntent.putExtra("email", authResult.user!!.email);
+                    startActivity(myIntent)
+                }
+
         }.addOnFailureListener {
             Toast.makeText(this, "Register error!", Toast.LENGTH_SHORT).show()
         }
